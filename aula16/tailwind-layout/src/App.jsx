38c23e3adf533c4a66c0121d757d94042854378f
Laboratório FIAP <labsfiap@fiap.com.br>
2026-04-23 22:36:05 -0300

    nova aula

--- aula16/tailwind-layout/src/App.jsx
+++ aula16/tailwind-layout/src/App.jsx
@@ -1,10 +1,13 @@
 import './App.css'
+import Button from './components/Button/Button'
 
 function App() {
 
   return (
     <>
-
+      <Button text="ola"/>
+      <Button text="adicionar"/>
+      <Button text="Cancelar"/>
     </>
   )
 }
